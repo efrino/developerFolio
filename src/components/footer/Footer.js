284247eeac82c434e537f4ec4b2ea8e24ba2b8c6
Efrino Wahyu Eko Pambudi <efrinowep@gmail.com>
2024-06-03 02:24:48 +0700

    edit footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,8 @@
-import React, {useContext} from "react";
-import "./Footer.scss";
-import {Fade} from "react-reveal";
+import React, { useContext } from "react";
 import emoji from "react-easy-emoji";
+import { Fade } from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
+import "./Footer.scss";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
@@ -10,12 +10,12 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made with ❤️ by Efrino Wahyu Eko Pambudi")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://github.com/efrino/developerFolio">
+            Efrino Wahyu
           </a>
         </p>
       </div>
